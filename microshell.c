c536f0946602f4c1dd6d77e3dc43992ca50ec8bf
Jean-Michel Dusserre <jedusser@z1r8p1.42lyon.fr>
2024-12-30 14:23:28 +0100

    microshell for github

--- microshell.c
+++ microshell.c
@@ -5,82 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 09:20:21 by jedusser          #+#    #+#             */
-/*   Updated: 2024/11/18 12:54:24 by jedusser         ###   ########.fr       */
+/*   Created: 2024/12/30 11:26:51 by jedusser          #+#    #+#             */
+/*   Updated: 2024/12/30 14:17:37 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-// si PAS DE PIPE et que la cmd produit un stdout :: rien.
-//si PIPE et que cmd produit un stdou :: stdout >> tmp;
-// tmp >> [stdin]prochaine commande[stdout]
-
-
-//  [STDIN]cmd1[STDOUT]
-
-//  PIPE : cration de tmp.txt
-    //--> read DONC creation de read fd
-    //-->  STDIN[read fd]STDOUT
-//  [STDIN]cmd2[STDOUT]
-
-// if (strncmp(argv[i + 1], "|", 1))
-// {
-//     handle_pipes();
-// }
-// // printf("Hello from child, pid = %d, executing: %s\n", pid, argv[i]);
-void print_tab(char **tab)
+void parsing(char **tab, char **argv)
 {
-    int i = 0;
-    while (tab[i])
-    {
-        printf("[%s]\n", tab[i]);
-        i++;
-    }
-    printf("\n");
+   
+    int i = 1;
+    int j = 0;
     
+    while (argv[i])
+        tab[j++] = argv[i++];
+    tab[j] = NULL;
+    
+    
+    //debug
+    j = -1;
+    while (tab[j++])
+        printf("tab[%d] = %s\n",j, tab[j]);
+    //debug
 }
+
+
 int main(int argc, char **argv, char **envp)
 {
-    int i;
-    pid_t pid;
-    int pipe_fds[2];
+    (void)argc;
+    char *tab[1024];
+    char **big_tab[1024];
     
-    // loop through argv to check for pipes or ;
+    parsing(tab, argv);
+
     
-    i = 1;
-    printf("tab at start : \n");
-    print_tab(argv);
-    while (i < argc)
+    int i = 0;
+    big_tab[i] = tab;
+    
+    
+
+    if (execve(big_tab[i][0], big_tab[0], envp) == -1)
     {
-        pid = fork();
-        if (pid == 0)
-        {
-            
-            if (execve(argv[i], &argv[i], envp) != -1)
-            {
-                printf("tab in execve : \n");
-                print_tab(&argv[i]);
-                
-                perror("execve");
-                exit(1);
-            }
-        }
-        else if (pid > 0)
-        {
-            // printf("hello from parent, pid = %d\n", pid);
-            wait(NULL);
-        }
-        else
-        {
-            perror("fork");
-            return 1;
-        }
-        i++;
+        perror("error :");
+        return (-1);
     }
-    return 0;
 }
